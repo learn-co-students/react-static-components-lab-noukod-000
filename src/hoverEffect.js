eb4041ff98dce1465be9598dafe5376da35fa054
carly <carly@noukod.com>
2019-03-26 17:14:03 +0000

    Done.

--- src/hoverEffect.js
+++ src/hoverEffect.js
@@ -1,5 +1,6 @@
 // This script is an altered version of the CodePen made by 'Captain Anonymous': https://codepen.io/anon/pen/jKQoaj
 
+
 const rippleSettings = {
   maxSize: 100,
   animationSpeed: 2,
@@ -43,7 +44,7 @@ Ripple.prototype = {
       ${Math.floor(rippleSettings.strokeColor[2])},
       ${this.opacity})`;
   },
-  
+
   draw: function draw() {
     this.ctx.beginPath();
     this.ctx.strokeStyle = this.strokeColor;
@@ -51,7 +52,7 @@ Ripple.prototype = {
       2 * Math.PI);
     this.ctx.stroke();
   },
-  
+
   setStatus: function setStatus(status) {
     this.status = status;
   },
